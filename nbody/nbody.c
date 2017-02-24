daa9413f3cf34aadb2b1a864a0e70f15a5815c64
Jay Timmer <jytmmr@beaker-hn-1.cm.cluster>
2017-02-24 13:22:18 -0600

    Added function

--- nbody/nbody.c
+++ nbody/nbody.c
@@ -13,72 +13,73 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define G 6.67*pow(10,-11) // the gravitational constant G
-#define DELTA_T 3600// time slice
+#define G 6.67e-11 // the gravitational constant G
+#define DELTA_T 3600 * 24// time slice
 
 struct Body{
 	//char name[] = "test";
 	double px, py; //cartesian coordinates
 	double vx, vy; //velocity
 	double fx, fy; // force
+	double accel_x, accel_y;
 	double r; // radius
 	double mass;
-
 };
 
+
+struct Body computePosition(struct Body body1, struct Body body2){
+	
+	double delta_x = body2.px - body1.px;
+	double delta_y = body2.py - body1.py;
+	double d = sqrt(pow(delta_x,2) + pow(delta_y, 2)); // distance between the bodies
+
+	double F = G*(body2.mass * body1.mass)/ pow(d,2);
+	double Fx = F*delta_x / d;
+	double Fy = F*delta_y / d;
+
+	body1.accel_x = Fx / body1.mass;
+	body1.accel_y = Fy/ body1.mass;
+
+	body1.vx = body1.vx + DELTA_T * body1.accel_x;
+	body1.vy = body1.vy + DELTA_T * body1.accel_y;
+
+	body1.px = body1.px + DELTA_T * body1.vx;
+	body1.py = body1.py + DELTA_T * body1.vy;
+	
+	return body1;
+}
 int main(){
-	//printf("HI");
 
 
 	//init the bodies
 	struct Body earth;
-	earth.px = 1.496 * pow(10,11);
+	earth.px = 1.496e11;
 	earth.py = 0;
 
 	earth.vx = 0;
-	earth.vy = 2.980* pow(10,11);
-	earth.mass = 5.972 * pow(10,24);
+	earth.vy = 2.980* pow(10,4);
+	earth.mass = 5.972e24;
 
 
 
 	struct Body sun;
 	sun.px, sun.py = 0;
 	sun.vx, sun.vy = 0;
-	sun.mass = 1.989 * pow(10,30);
+	sun.mass = 1.989e30;
 
 
 
 
 	int i;
-	for(i = 0; i < 24*365; i++){
-		printf("\n");
+	for(i = 0; i < 365; i++){
+		printf("%d\n",i);
 		printf("   EARTH: p(%f,%f) with v(%f,%f) \n",earth.px, earth.py, earth.vx, earth.vy);
 		printf("   SUN: p(%f,%f) with v(%f,%f) \n",sun.px, sun.py, sun.vx, sun.vy);
+		struct Body tempEarth = earth;
+		earth = computePosition(earth, sun);
+		sun = computePosition(sun, tempEarth);
+		
 
-		double delta_x = sun.px - earth.px;
-		double delta_y = sun.py - earth.py;
-		double d = sqrt(pow(delta_x,2) + pow(delta_y, 2)); // distance between the bodies
-
-		double F = G*(earth.mass *sun.mass)/ pow(d,2);
-		double Fx = F*delta_x / d;
-		double Fy = F*delta_y / d;
-
-		double ax1 = Fx / earth.mass;
-		double ay1 = Fy/ earth.mass;
-		double ax2 = Fx / sun.mass;
-		double ay2 = Fy/ sun.mass;
-
-		earth.vx = earth.vx + DELTA_T * ax1;
-		earth.vy = earth.vy + DELTA_T * ay1;
-
-		earth.px = earth.px + DELTA_T * earth.vx;
-		earth.py = earth.py + DELTA_T * earth.vy;
-
-		sun.vx = sun.vx + DELTA_T * ax2;
-		sun.vy = sun.vy + DELTA_T * ay2;
-
-		sun.px = sun.px + DELTA_T * sun.vx;
-		sun.py = sun.py + DELTA_T * sun.vy;
 	}
 
 }
